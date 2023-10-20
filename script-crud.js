d2d702eba61ec8ddc7a52272a51fb2e1a5d813aa
Vinicios Neves <vinicios_neves@hotmail.com>
2023-10-20 19:20:56 +0100

    Video 1.3

--- script-crud.js
+++ script-crud.js
@@ -1,9 +1,19 @@
 const formularioTarefa = document.querySelector('form.app__form-add-task')
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const legendaFormulario = document.querySelector('.app__form-label')
+const textarea = document.querySelector('.app__form-textarea')
+
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formularioTarefa.classList.toggle('hidden')
 })
 
-
+formularioTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const descricao = textarea.value
+    tarefas.push({
+        descricao
+    })
+    localStorage.setItem('tarefas', tarefas)
+})
